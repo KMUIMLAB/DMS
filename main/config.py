71ccfc994731133cbe1532e654ad38a0fec88bc3
SeungYeob <julio981007@kookmin.ac.kr>
2024-05-23 17:59:51 +0900

    version update in config.py

--- main/config.py
+++ main/config.py
@@ -1,5 +1,5 @@
 config = {'SAVE_PATH': '/media/imlab/Samsung_T5/dms_rev1/',
-          'VERSION': 'v1.3.0',
+          'VERSION': 'v1.4.0',
           'DRIVER_LIST': ["Sejoon", "Geesung",\
                           "Rakcheol", "Youngjun",\
                           "Seungyeob", "Chanyoung"],
